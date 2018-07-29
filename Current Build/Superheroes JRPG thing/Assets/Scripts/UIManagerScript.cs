a991df5b21dcd0d8de60e7f0b80d7d08c77a2ed8
Nick Favia <xDarkWolfv@gmail.com>
2018-07-29 00:30:57 -0500

    7/28 Starting Dialog System
    mad it so that when you walk up to an "NPC" a symbol will show up in the corner indicating that the player can talk with said npc. Not much else ws done from that point as I got tired and bored.

---
+++ Current Build/Superheroes JRPG thing/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public Canvas theCanvas;
+
+    public Image talkImg;
+
+    public bool exists;
+    public static bool talking;
+    
+    // Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (talking)
+        {
+            if (!exists)
+            {
+                Image talkImgTemp = (Image)Instantiate(talkImg, theCanvas.transform);
+                talkImgTemp.transform.SetParent(theCanvas.transform);
+                talkImgTemp.name = "temp";
+                exists = true;
+            }
+        }
+        else
+        {
+            Destroy(GameObject.Find("temp"));
+            exists = false;
+        }
+            
+	}
+
+    public static bool InteractTalk()
+    {
+        if(!talking)
+            talking = true;
+        return talking;
+    }
+
+    public static bool StopInteract()
+    {
+        if (talking)
+            talking = false;
+        return talking;
+    }
+}
